688d38957cd3c43084c9431dc6f41d56f5b46902
TutorialHell <agrablewski@gmail.com>
2026-05-22 05:37:19 -0400

    list products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/header/product/product-list";
+import sampleData from "@/db/sample-data"
 
 const Homepage = () => {
-  return ( <>Sportstore
+  
+  return ( <>
+    <ProductList  data={sampleData.products} title="Newest Arrivals" limit={4} />
   </> );
 }
  
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ShoppingCart, UserIcon, EllipsisVertical } from 'lucide-react';
+import ModeToggle from './mode-toggle';
+import { Sheet, SheetTitle, SheetDescription, SheetTrigger, SheetContent} from '@/components/ui/sheet'
+
+const Menu = () => {
+    return ( <div className="flex justify-end gap-3">
+                <nav className="hidden md:flex w-full max-2-xs gap-1">
+                    <ModeToggle />
+                        <Button asChild variant="ghost">
+                            <Link href="/cart">
+                                <ShoppingCart /> Cart
+                            </Link>
+                         </Button>
+                         <Button asChild className="bg-black text-white hover:bg-black/80 dark:bg-primary dark:text-primary-foreground dark:[a]:hover:bg-primary/80">
+                            <Link href="/sign-in">
+                             <UserIcon /> Sign In
+                             </Link>
+                         </Button>
+                </nav>
+                <nav className="md:hidden">
+                    <Sheet>
+                        <SheetTrigger className="align-middle">
+                            <EllipsisVertical />
+                            <SheetContent className="flex flex-col items-start">
+                                <SheetTitle>Menu</SheetTitle>
+                                <ModeToggle />
+                                <Button asChild variant="ghost">
+                                    <Link href="/cart">
+                                      <ShoppingCart /> Cart
+                                    </Link>
+                                </Button>
+                                <Button asChild className="bg-black text-white hover:bg-black/80 dark:bg-primary dark:text-primary-foreground dark:[a]:hover:bg-primary/80">
+                                    <Link href="/sign-in">
+                                        <UserIcon /> Sign In
+                                    </Link>
+                                </Button>
+                            <SheetDescription></SheetDescription>
+                            </SheetContent>
+                        </SheetTrigger>
+                    </Sheet>
+                </nav>
+    </div> );
+}
+ 
+export default Menu;
