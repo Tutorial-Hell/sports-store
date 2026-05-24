8a6815a9aed80b8b541c63233f1446f7120e84b3
TutorialHell <agrablewski@gmail.com>
2026-05-24 07:55:48 -0400

    add product pages

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,10 +3,18 @@ import { prisma } from '@/db/prisma'
 import { convertToPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '../constants'
 
+// Get Latest Product
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy: { createdAt: 'desc' },
     })
     return convertToPlainObject(data)
+}
+
+// Get single product from slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: {slug: slug}
+    })
 }
